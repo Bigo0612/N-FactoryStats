5a27be86c6cb6af83237ead81793064a4005f4f9
Bigo0612 <quesada.steve@gmail.com>
2020-01-13 09:35:15 +0100

    Creation dossier flexslider, footer fini, header presque fini; manque le bon logo, section 1 et 2 en cours. Style. css en cours.

--- inc/header.php
+++ inc/header.php
@@ -2,9 +2,22 @@
 <html lang="fr" dir="ltr">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Ibarra+Real+Nova&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="asset/flexslider/flexslider.css" type="text/css">
+    <link rel="stylesheet" href="asset/css/style.css">
     <title>N'FactoryStats</title>
 </head>
-<body>
 
-</body>
-</html>
+<header>
+    <div class="fixmenu">
+        <div class="header-acceuil">
+            <a id="logo" href="index.php"><img class="imglogo" src="asset/img/N'FactoryStats.png" alt="image de logo N\'FactoryStats"></a>
+            <a href="#quisommesnous"><p>Qui sommes nous ?</p></a>
+            <a href="#contact"><p>Contactez nous</p></a>
+            <a href="#"><p>Connexion</p></a>
+            <a class= inscri href="#"><p>Inscription</p></a>
+        </div>
+    </div>
+    <div class="clear"></div>
+</header>
