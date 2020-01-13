ad5e9afa31cbf500470729adf08f63185c570d2b
Bigo0612 <quesada.steve@gmail.com>
2020-01-13 15:19:44 +0100

    Modification index.php

--- index.php
+++ index.php
@@ -4,26 +4,23 @@ include ('inc/pdo.php');
 $errors = array();
 $success = false;
 if (!empty($_POST['submit'])) {
-    $nom             = clean($_POST['nom']);
-    $prenom          = clean($_POST['prenom']);
     $email           = clean($_POST['email']);
-    $message         = clean($_POST['message']);
+    $password        = clean($_POST['password']));
+    $password2       = clean($_POST['password2']));
 
 
-    $errors = textValid($errors, $nom, 'nom',2, 140);
-    $errors = textValid($errors, $prenom, 'prenom',2, 150);
     $errors = emailValid($errors, $email, 'email',5,255);
-    $errors = textValid($errors, $message, 'message',10,2000);
+    $error['password']  = $formVerif->errorText($password, 'Mot de passe', 5, 255);
+    $error['password2'] = $formVerif->errorRepeat($password, $password2, 'Les mots de passe ne correspondent pas.');
 
 
     if (count($errors) == 0) {
         $success = true;
-        $sql = "INSERT INTO contact VALUES(null ,:nom ,:prenom ,:email ,:message)";
+        $sql = "INSERT INTO contact VALUES(null ,:email ,:password ,:password2)";
         $query = $pdo->prepare($sql);
-        $query->bindValue(':nom', $nom, PDO::PARAM_STR);
-        $query->bindValue(':prenom', $prenom, PDO::PARAM_STR);
         $query->bindValue(':email',$email, PDO::PARAM_STR);
-        $query->bindValue(':message',$message, PDO::PARAM_STR);
+        $query->bindValue(':password', $password, PDO::PARAM_STR);
+        $query->bindValue(':password2', $password2, PDO::PARAM_STR);
         $query->execute();
     }
 }
@@ -63,50 +60,7 @@ include('inc/header.php'); ?>
                     deserunt dolor excepturi hic illo ipsa modi qui totam. Minus molestiae quae repudiandae vero.</p>
             </div>
         </div>
-
     </section>
-
-    <section id="contact">
-        <div class="form">
-            <div class="wrap3">
-                <h2>Nous contacter</h2>
-                <div class="ligne"></div>
-                <h3>Envoyez votre message</h3>
-            <?php if($success) {
-                header('Location: success.php');
-            } else { ?>
-                <form action="" method="post">
-                    <div id="titi" class="w50">
-                        <label for="name">Votre Nom</label>
-                        <input id="nom" type="text" name="nom" placeholder="Ex: Tarantino" value="<?php if (!empty($_POST['nom'])) {echo $_POST['nom'];} ?>">
-                        <span class="error"><?php if(!empty($errors['nom'])) {echo $errors['nom']; }?></span>
-                    </div>
-
-                    <div id="tintin" class="w50">
-                        <label for="name">Votre Prenom</label>
-                        <input id="prenom" type="text" name="prenom" placeholder="Ex: Quentin" value="<?php if (!empty($_POST['prenom'])) {echo $_POST['prenom'];} ?>">
-                        <span class="error"><?php if(!empty($errors['prenom'])) {echo $errors['prenom']; }?></span>
-                    </div>
-
-                    <div class="w50">
-                        <label for="name">Votre Email</label>
-                        <input id="email" type="email" name="email" placeholder="Ex: quentintarantino@gmail.com" value="<?php if (!empty($_POST['email'])) {echo $_POST['email'];} ?>">
-                        <span class="error"><?php if(!empty($errors['email'])) {echo $errors['email']; }?></span>
-                    </div>
-
-                    <div class="w100">
-                        <label for="name">Votre Message</label>
-                        <textarea name="message" rows="8" cols="72" placeholder="Bonjour"><?php if (!empty($_POST['message'])) {echo $_POST['message'];} ?></textarea>
-                        <span class="error"><?php if(!empty($errors['message'])) {echo $errors['message']; }?></span>
-                    </div>
-
-                    <input type="submit" name="submit" value="Envoyer">
-                </form> <?php } ?>
-            </div>
-        </div>
-        <div class="clear"></div>
-    </section>
-
 </div>
 
 
