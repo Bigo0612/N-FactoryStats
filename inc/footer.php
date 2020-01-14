f7c1056c2f92516855ac4d412c71c5ce3fd91ee2
Bigo0612 <quesada.steve@gmail.com>
2020-01-14 15:01:30 +0100

    Function textvalide, emailvalide, clean et debug. name pdo html du footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,12 @@
 </div>
 
 <footer>
-
+    <div class="wrap4">
+        <p>&copy;2020 - N'FactoryStats &reg;</p>
+        <a class="je" href="index.php">Accueil</a>
+        <a class="tu" href="#">Politiques de confidentialités</a>
+        <a class="il" href="#">Mentions légales</a>
+    </div>
 </footer>
 
 </body>
