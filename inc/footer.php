9c9edc32bf584581a781958d2ac039f874fbf9e2
Bigo0612 <quesada.steve@gmail.com>
2020-01-17 12:22:41 +0100

    modif lien

--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,7 @@
         <p>&copy; 2020 - N'FactoryStats &reg;</p>
         <a href="../index.php">Accueil</a>
         <a href="#">Politiques de confidentialités</a>
-        <a href="../mentions-legales.php">Mentions légales</a>
+        <a href="mentions-legales.php">Mentions légales</a>
     </div>
 </footer>
 
