859b0a0991659b1f929eeddb770641c6df0f1b7c
Bigo0612 <quesada.steve@gmail.com>
2020-01-17 09:21:08 +0100

    correction mention legales

--- mentions-legales.php
+++ mentions-legales.php
@@ -1,6 +1,6 @@
 <?php include "header.php"; ?>
 
-<link rel="stylesheet" href="asset/css/Mentions-légales.css">
+<link rel="stylesheet" href="asset/css/Mentions-legales.css">
 
 <div class="Mentions-légales">
 
