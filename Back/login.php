94a9599b880928d5993b8fe85adb58d4d1a9454c
Bigo0612 <quesada.steve@gmail.com>
2020-01-17 01:36:29 +0100

    petit commit^^

--- Back/login.php
+++ Back/login.php
@@ -16,29 +16,29 @@ if(!empty($_POST['submitted'])) {
     } else {
         $sql = "SELECT * FROM users WHERE email=:email";
         $query = $pdo->prepare($sql);
-$query -> bindValue(':email',$email,PDO::PARAM_STR);
-$query->execute();
-$user = $query->fetch();
-if(!empty($user)) {
+        $query -> bindValue(':email',$email,PDO::PARAM_STR);
+        $query->execute();
+        $user = $query->fetch();
+        if(!empty($user)) {
 
-if(password_verify($password,$user['password'])) {
+            if(password_verify($password,$user['password'])) {
 
-$_SESSION['email'] = array(
-'id'    => $user['id'],
-'email'=> $user['email'],
-'role'  => $user['role'],
-'ip'    => $_SERVER['REMOTE_ADDR']
-);
+                $_SESSION['email'] = array(
+                    'id'    => $user['id'],
+                    'email'=> $user['email'],
+                    'role'  => $user['role'],
+                    'ip'    => $_SERVER['REMOTE_ADDR']
+                );
 
-header('Location: index.php');
+                header('Location: index.php');
 
-} else {
-$errors['email'] = 'email inconnu ou mot de passe oublié';
-}
-} else {
-$errors['email'] = 'email inconnu';
-}
-}
+            } else {
+                $errors['email'] = 'email inconnu ou mot de passe oublié';
+            }
+        } else {
+            $errors['email'] = 'email inconnu';
+        }
+    }
 }
 
 
