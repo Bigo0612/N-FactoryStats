8bf3d1b42c7b3ca55a18cfb26156991478b95831
Bigo0612 <quesada.steve@gmail.com>
2020-01-17 12:18:17 +0100

    lien sur le footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -2,9 +2,9 @@
 <footer>
     <div class="wrap4">
         <p>&copy; 2020 - N'FactoryStats &reg;</p>
-        <a href="index.php">Accueil</a>
+        <a href="../index.php">Accueil</a>
         <a href="#">Politiques de confidentialités</a>
-        <a href="Mentions-légales.php">Mentions légales</a>
+        <a href="../mentions-legales.php">Mentions légales</a>
     </div>
 </footer>
 
