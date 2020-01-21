168f8f1b7b97acd44d925f0937e65516952e70a9
Petro Moute <melie.deneuve@outlook.com>
2020-01-21 15:12:49 +0100

    contact avec sa bd

--- inc/footer.php
+++ inc/footer.php
@@ -3,7 +3,8 @@
     <div class="wrap4">
         <p>&copy; 2020 - N'FactoryStats &reg;</p>
         <a href="../index.php">Accueil</a>
-        <a href="#">Politiques de confidentialités</a>
+        <a href="contact.php">Contact</a>
+        <a href="#">Politique de confidentialité</a>
         <a href="mentions-legales.php">Mentions légales</a>
     </div>
 </footer>
