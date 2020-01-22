e42104e4a0e177b3c3a329f37ee9d5ee45b9fa88
Petro Moute <melie.deneuve@outlook.com>
2020-01-22 09:21:18 +0100

    cgu dans footer

--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,7 @@
         <p>&copy; 2020 - N'FactoryStats &reg;</p>
         <a href="../index.php">Accueil</a>
         <a href="contact.php">Contact</a>
-        <a href="#">Politique de confidentialité</a>
+        <a href="cgu.php">CGU</a>
         <a href="mentions-legales.php">Mentions légales</a>
     </div>
 </footer>
