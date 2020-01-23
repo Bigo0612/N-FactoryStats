0281e63fa8a810d81306486f59b579f41c5d581d
Bigo0612 <quesada.steve@gmail.com>
2020-01-23 11:55:53 +0100

    modif

--- 404.php
+++ 404.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="asset/css/404.css">
-<img class="licorne-img" src="asset/img/erreur-404-2.jpg" alt="">
+<img class="404" src="asset/img/erreur-404-2.jpg" alt="">
 <?php
 session_start();
 echo '<p class="licorne">Error 404 Not Found</p>';
