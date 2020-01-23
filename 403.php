7a8a63e789ba20e2d56db46a08ff90affa00a3fb
Bigo0612 <quesada.steve@gmail.com>
2020-01-23 11:55:32 +0100

    modif

--- 403.php
+++ 403.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="asset/css/403.css">
-<img class="licorne-img" src="asset/img/erreur-403.jpg" alt="">
+<img class="403" src="asset/img/erreur-403.jpg" alt="">
 <?php
 session_start();
 echo "<p>Error 403 Vous n'avez pas accès à cette page.</p>";
